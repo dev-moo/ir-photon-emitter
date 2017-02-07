ec7ef90e29ea01cdd7780ab9fcc176bcd990d9bc
Andy <AMoore@fqml.com>
2017-02-07 16:45:43 +0800

    upload python scripts

--- irpe.py
+++ irpe.py
@@ -1,112 +1,245 @@
 #!/usr/bin/env python
 
-#IR Photon Emitter Service
+"""
+IR Photon Emitter Service
+UPD server to operate IR light
+"""
 
-import RPi.GPIO as GPIO
-import select
-import socket
-import json
 import sys
+import socket
+import select
+import json
+import threading
+import logging
+from time import sleep
+import RPi.GPIO as GPIO
 
-sys.path.insert(0, '/var/scripts/ir-photon-emitter')
+HOMEPATH = '/var/scripts/ir-photon-emitter'
 
+sys.path.insert(0, HOMEPATH)
 import irpe_config
 
-config = irpe_config.IRPE_Config()
+CONFIG = irpe_config.IRPE_Config()
 
+logging.basicConfig(filename=HOMEPATH + CONFIG.logfile, level=logging.DEBUG)
 
+logging.debug('ir-photon-emitter service starting')
 
 #Pin Definitons
-pin1 = 17
-pin2 = 18
+PIN1 = 17
+PIN2 = 18
 
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(pin1, GPIO.OUT)
-GPIO.setup(pin2, GPIO.OUT) 
+GPIO.setup(PIN1, GPIO.OUT)
+GPIO.setup(PIN2, GPIO.OUT)
 
-GPIO.output(pin1, GPIO.LOW)
-GPIO.output(pin2, GPIO.LOW)
+GPIO.output(PIN1, GPIO.LOW)
+GPIO.output(PIN2, GPIO.LOW)
 
-
-
-# Create a UDP socket 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setblocking(0)
-server_address = (config.serverIP, config.serverPort)
-		
-# Bind the socket to the port	
-while True:
-	try: 
-		sock.bind(server_address)
-		break
-	except:
-		sleep(5)
-		
-		
-		
-def TurnOn(level):
-	GPIO.output(pin1, GPIO.HIGH)
-	GPIO.output(pin2, GPIO.HIGH)
-
-def TurnOn_Half(level):
-	GPIO.output(pin1, GPIO.HIGH)
-	GPIO.output(pin2, GPIO.LOW)
-
-def TurnOff():
-	GPIO.output(pin1, GPIO.LOW)
-	GPIO.output(pin2, GPIO.LOW)
-
-	
-		
-		
+#Globals / variables
+STATE = 'Off'
+MAXTIME = 900 #seconds
+TIMERTHREADNAME = 'IRTIMER'
+IRTIMER = None
 
 
-#Inputs to monitor
-inputs = [sock]
+def start_timer():
+    """start timer thread to turn off ir light after MAXTIME
+    args: None
+    returns : None
+    """
 
-#Main Loop
+    global IRTIMER
+
+    IRTIMER = threading.Timer(MAXTIME, turn_off)
+    IRTIMER.setName(TIMERTHREADNAME)
+    IRTIMER.start()
+
+
+def check_timer():
+
+    """Check if Timer thread is running
+    args: None
+    returns: Boolean
+    """
+
+    for tds in threading.enumerate():
+
+        if tds.getName() == TIMERTHREADNAME:
+            return True
+
+    return False
+
+
+def set_timer():
+
+    """Start timer if not running, otherwise reset"""
+
+    global IRTIMER
+
+    if check_timer():
+        IRTIMER.cancel()
+        start_timer()
+    else:
+        start_timer()
+
+
+def turn_on():
+
+    """turn on IR light at full power"""
+
+    global STATE
+
+    STATE = 'High'
+
+    GPIO.output(PIN1, GPIO.HIGH)
+    GPIO.output(PIN2, GPIO.HIGH)
+
+    set_timer()
+
+    logging.debug('turn_on')
+
+
+def turn_on_half_1():
+
+    """Turn on IR light at half power"""
+
+    global STATE
+
+    STATE = 'Low'
+
+    GPIO.output(PIN1, GPIO.HIGH)
+    GPIO.output(PIN2, GPIO.LOW)
+
+    set_timer()
+
+    logging.debug('turn_on_half_1')
+
+
+def turn_on_half_2():
+
+    """Turn on IR light at half power"""
+
+    global STATE
+
+    STATE = 'Low'
+
+    GPIO.output(PIN1, GPIO.LOW)
+    GPIO.output(PIN2, GPIO.HIGH)
+
+    set_timer()
+
+    logging.debug('turn_on_half_2')
+
+
+def turn_off():
+
+    """turn off IR light"""
+
+    global STATE, IRTIMER
+
+    STATE = 'Off'
+
+    GPIO.output(PIN1, GPIO.LOW)
+    GPIO.output(PIN2, GPIO.LOW)
+
+    IRTIMER.cancel()
+
+    logging.debug('turn_off')
+
+
+def operate(cmd):
+
+    """parse operations received via JSON over UDP"""
+
+    if cmd['Power'] == 'Off':
+        turn_off()
+
+    elif cmd['Power'] == 'Low':
+        turn_on_half_1()
+
+    elif cmd['Power'] == 'Off':
+        turn_on()
+
+
+def get_state():
+
+    """return current start of IR light"""
+    return {'Power': STATE}
+
+
+def setup_socket(s_ip, s_port):
+
+    """Create UDP socket to communicate on the network"""
+
+    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sck.setblocking(0)
+    server_address = (s_ip, s_port)
+
+    # Bind the socket to the port
+    while True:
+        try:
+            sck.bind(server_address)
+            break
+        except:
+            logging.exception("Exception Occured:")
+            sleep(5)
+
+    return sck
+
+
+
+#Main Loop to send/receive UDP packets
+
 if __name__ == "__main__":
 
-	while inputs:
-	
-		# Wait for at least one of the sockets to be ready for processing
-		readable, writable, exceptional = select.select(inputs, [], inputs)
-		
-		# Handle inputs
-		for s in readable:
-		
-			#UPD Socket
-			if s is sock:
+    sock = setup_socket(CONFIG.server_ip, CONFIG.server_port)
 
-				data, address = sock.recvfrom(4096)
+    #Inputs to monitor
+    inputs = [sock]
 
-				print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    try:
 
-				if data:
-					try:
-						command = json.loads(data)
+        while inputs:
 
-						if command['Operation'] == "GET":
-						
-							dbg("GET command received")
-							
-							#Get settings and encode to JSON
-							json_str = json.dumps(get_status())
-							
-							sent = sock.sendto(json_str, address)	
+            # Wait for at least one of the sockets to be ready for processing
+            readable, writable, exceptional = select.select(inputs, [], inputs)
 
-							#dbg('sent %s bytes back to %s' % (sent, address))
-							dbg("")						
+            # Handle inputs
+            for s in readable:
 
-						if command['Operation'] == "SET":
-								
-							dbg("SET command received")
-							#print "Command: " + command
-							operate(command)
-							
-							dbg("")                                             
+                #UPD Socket
+                if s is sock:
 
-					except:
-							dbg('Error receiving UPD JSON data')
-							log_event("An Exception Occured")			
-		
+                    data, address = sock.recvfrom(4096)
+
+                    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+
+                    if data:
+
+                        try:
+                            command = json.loads(data)
+
+                            if command['Operation'] == "GET":
+
+                                #Get settings and encode to JSON
+                                json_str = json.dumps(get_state())
+
+                                sent = sock.sendto(json_str, address)
+
+                            if command['Operation'] == "SET":
+
+                                operate(command)
+
+
+                        except:
+                            logging.exception("Exception Occured:")
+                            print "Error"
+
+    except:
+        logging.exception("Exception Occured:")
+
+    finally:
+        GPIO.cleanup()
+        sock.close()
+        logging.debug('ir-photon-emitter service shut down')
--- irpe_config.py
+++ irpe_config.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-
+"""Config file for irpe.py"""
 
 class IRPE_Config():
 
-	def __init__(self):
-	
-		self.serverIP = '192.168.1.3'
-		self.serverPort = 10111
+    def __init__(self):
+
+        self.server_ip = localhost
+        self.server_port = 11111
+        self.logfile = "irpe_log.txt"
