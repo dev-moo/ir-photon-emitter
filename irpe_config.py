adaf34d3119717319543d16b4151bb6f37d09bce
Andy <mail4andrewm@gmail.com>
2021-09-14 19:46:29 +0800

    latest commit

--- irpe_config.py
+++ irpe_config.py
@@ -4,6 +4,6 @@ class IRPE_Config():
 
     def __init__(self):
 
-        self.server_ip = localhost
+        self.server_ip = "192.168.1.3"
         self.server_port = 11111
         self.logfile = "irpe_log.txt"
